33fba481bf9513976bf174a763774c71176eda37
Christopher Elliott <christopher.elliott8@gmail.com>
2017-10-13 09:31:37 -0500

    Commit #1
    Initial Commit...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# LIRI
-LIRI is a Language Interpretation &amp; Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+#LIRI - Language Interpretation and Recognition Interface
+
+## Introduction
+LIRI is like SIRI (from iOS).  It is a command line node app that takes in parameters and outputs data.
---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+  consumer_key: "bU2BSla1o2Fq5i2G5zXjSLotU",
+  consumer_secret: "9nKWvRNXk6nuWk3EZ3dmBUQ5FjHFR7ykQfd0rnAZT66tJyBUMX",
+  access_token_key: "916795491408662529-HiSnQt7YwBz333iJ0TNpF9sFcGu19Yb",
+  access_token_secret: "Rc5AYXqaimDsMNXwNpy7tlNfzzLfnYDBdcR07NK4UmzPb"
+};
+
+module.exports = twitterKeys;
